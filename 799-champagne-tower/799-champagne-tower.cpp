78f888816b14df37236bb404fa91da6fe24277c7
Vishal Tiwari <itsvishal.codes@gmail.com>
2022-03-05 00:43:27 +0530

    Time: 32 ms (25.32%), Space: 42.6 MB (21.26%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
   double champagneTower(int poured, int query_row, int query_glass) {
-    vector<vector<double>> fractionFilled(101, vector<double>(101, 0.0));
+    vector<vector<double>> fractionFilled(100, vector<double>(100, 0.0));
     fractionFilled[0][0] = (double)poured;
     
     double flow;    
-    for(int i=0; i<= query_row; i++) {
+    for(int i=0; i< query_row; i++) {
       for(int it=0; it <= i; it++) {
         flow = (fractionFilled[i][it] - 1.0) / 2.0;
         if(flow > 0) {
