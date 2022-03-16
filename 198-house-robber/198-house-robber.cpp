e11bda9c699b02b1c11e2d4b506c90a2210e53f8
Vishal Tiwari <itsvishal.codes@gmail.com>
2022-03-16 10:12:09 +0530

    Time: 4 ms (35.13%), Space: 7.6 MB (76.25%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-  int totalAmount(vector<int>& nums, int idx, unordered_map<int, int>& dp) {
-    // Base Case
+  int robOrNot(vector<int>& nums, int idx, vector<int>& dp) {
     if(idx >= nums.size()) return 0;
     
-    if(dp.find(idx) != dp.end()) return dp[idx];
+    // Either Rob This House or Rob the next one
+    if(dp[idx] != -1) return dp[idx];
     
-    // 2 options in recursive case
-    // 1. Rob the current house but then you can't rob the next house
-    int house1 = nums[idx] + totalAmount(nums, idx+2, dp);
-    // 2. Ignore the current house and rob the next one
-    int house2 = totalAmount(nums, idx+1, dp);
+    int currentHouse = nums[idx] + robOrNot(nums, idx+2, dp);
+    int nextHouse = robOrNot(nums, idx+1, dp);
     
-    // We'll take max of it
-    int totalEarning = max(house1, house2);
-    
-    dp[idx] = totalEarning;
-    
-    return dp[idx];
+    return dp[idx] = max(currentHouse, nextHouse);
   }
   
   int rob(vector<int>& nums) {
-    unordered_map<int, int> dp;
-    return totalAmount(nums, 0, dp);
+    vector<int> dp(nums.size()+1, -1);
+    return robOrNot(nums, 0, dp);
   }
 };
