c404640c1a8aab5ee215ab63178f0f800aa616dd
Vishal Tiwari <itsvishal.codes@gmail.com>
2022-03-25 09:51:23 +0530

    Time: 7 ms (6.60%), Space: 5.8 MB (81.30%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  double calc(double x, int n) {
+    if(n==0) return 1;
+    if(n==1) return x;
+    
+    double soln = pow(calc(x, n/2), 2);
+    if(n%2) return soln*x;
+    return soln;
+  }
+  
+  double myPow(double x, int n) {    
+    if(n == 0) return 1;
+    
+    if(n>0) return calc(x, n);
+    return 1/calc(x, abs(n));
+  }
+};
