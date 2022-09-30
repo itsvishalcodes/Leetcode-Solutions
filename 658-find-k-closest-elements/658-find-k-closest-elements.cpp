8a7e3fa9e78539e80cca48742adae8a763eab1d5
Vishal Tiwari <itsvishal.codes@gmail.com>
2022-09-30 09:55:53 +0530

    Time: 91 ms (61.76%), Space: 31.5 MB (77.67%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,50 +1,21 @@
 class Solution {
 public:
   vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-    vector<int> diff(arr.size()), soln;
-    deque<int> solndeque;
-    int start, end, nearest=INT_MAX, zeroLoc = -1;
-    for(int i=0; i<arr.size(); i++) {
-      diff[i] = arr[i]-x;
-      if(diff[i] == 0) zeroLoc = i;
-    }
+    int start = 0, end = arr.size()-1;
+    int totalIt = arr.size()-k;
+    vector<int> soln;
     
-    for(int i=0; i<diff.size(); i++) {
-      if(min(nearest, abs(0-diff[i])) < nearest){
-        zeroLoc = i;
-        nearest = abs(0 - diff[i]);
-      }
-      
-    }
-    
-    k = k-1;
-    solndeque.push_back(arr[zeroLoc]);
-    start = zeroLoc-1;
-    end = zeroLoc+1;
-    while(k>0) {
-      if(start >=0 and end < arr.size()) {
-        if(abs(diff[start]) <= abs(diff[end])) {
-          solndeque.push_front(arr[start]);
-          start--;
-        }
-        else {
-          solndeque.push_back(arr[end]);
-          end++;
-        }
-      }
-      else if(start >= 0) {
-        solndeque.push_front(arr[start]);
-        start--;
+    while(totalIt--) {
+      if(abs(x-arr[start]) <= abs(x-arr[end])) {
+        end--;
       }
       else {
-        solndeque.push_back(arr[end]);
-        end++;
+        start++;
       }
-      k--;
     }
     
-    for(auto i:solndeque) {
-      soln.push_back(i);
+    for(int i=start; i<=end; i++) {
+      soln.push_back(arr[i]);
     }
     
     return soln;
