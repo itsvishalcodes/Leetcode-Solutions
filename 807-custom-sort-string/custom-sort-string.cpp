4412f53e3b8de05b144b9c3980b93d9e654878cb
Vishal Tiwari <itsvishal.codes@gmail.com>
2024-03-11 17:38:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (35.04%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
   string customSortString(string order, string s) {
-    map<char, int> pos;
+    vector<int> pos(26, 0);
     for(int i=0; i<order.size(); i++) {
-      pos[order[i]] = (i+1);
+      pos[order[i]-'a'] = (i+1);
     }
 
     sort(s.begin(), s.end(), [&pos](const char& c1, const char& c2) {
-      return pos[c1] < pos[c2];
+      return pos[c1-'a'] < pos[c2-'a'];
     });
     return s;
   }
