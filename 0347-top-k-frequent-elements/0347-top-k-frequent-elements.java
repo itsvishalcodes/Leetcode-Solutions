77d5fab1e2023ccc0f119416a95aa69ac1b7e410
Vishal Tiwari <itsvishal.codes@gmail.com>
2026-05-03 21:39:00 +0530

    Time: 18 ms (13.37%), Space: 47.3 MB (95.11%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,38 @@
+
+import java.util.AbstractMap;
+
+/*
+ * @lc app=leetcode id=347 lang=java
+ *
+ * [347] Top K Frequent Elements
+ */
+
+// @lc code=start
+class Solution {
+  public int[] topKFrequent(int[] nums, int k) {
+    int[] res = new int[k];
+    Map<Integer, Integer> mp = new HashMap<>();
+    Queue<Map.Entry<Integer, Integer>> freqPQ = new PriorityQueue<>(
+      (a, b) -> b.getValue() - a.getValue()
+    );
+    
+    for (int i:nums) {
+      if (!mp.containsKey(i)) {
+        mp.put(i, 0);
+      }
+      mp.put(i, mp.get(i)+1);
+    }
+
+    for (int i:mp.keySet()) {
+      freqPQ.add(new AbstractMap.SimpleEntry<Integer, Integer>(i, mp.get(i)));
+    }
+
+    for (int i=0; i<k; i++) {
+      res[i] = freqPQ.poll().getKey();
+    }
+
+    return res;
+  }
+}
+// @lc code=end
+
