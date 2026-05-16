a52b611b6a95ed33797799f829bbe7ffe6999269
Vishal Tiwari <itsvishal.codes@gmail.com>
2026-05-17 03:00:39 +0530

    Time: 6 ms (69.07%), Space: 46 MB (59.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -11,11 +11,23 @@ import java.util.Map;
  */
 
 // @lc code=start
+
+/*
+MADE MULTIPLE ERRORS IN THIS PROBLEM
+I was clearing all the items to the left of leftPtr one by one
+when moving the leftPtr which pushed the worst case time complexity
+to O(n-squared) and also caused bugs where I ended up clearing 
+characters that were already there in the sliding window.
+It's better to check if the character occured after the leftPtr and update
+if it did, otherwise ignore it.
+*/
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int maxLen = 1;
         int ptrLeft = 0, ptrRight = 1;
 
+        if (s == null) return 0;
         if (s.length() < 2) return s.length();
 
         Map<Character, Integer> charToIdx = new HashMap<>();
@@ -31,7 +43,6 @@ class Solution {
             }
             charToIdx.put(s.charAt(ptrRight), ptrRight);
             maxLen = Math.max(maxLen, ptrRight - ptrLeft + 1);
-            System.out.println("Largest String: "+s.substring(ptrLeft, ptrRight+1));
             ptrRight++;
         }
 
